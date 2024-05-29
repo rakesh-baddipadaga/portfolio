6529dbb9a3f591043daecd977b6137a2ff1a4073
Rakesh Baddipadaga <rakeshbaddipadaga8@gmail.com>
2024-05-30 04:17:24 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -11,20 +11,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-
-
 
 app.use('/api/profile', userRoutes);
 app.use('/api/image', imageRoutes);
 app.use('/api/vcard',vcardRoutes);
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {console.log('MongoDB connected')
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
+})
+.catch(err => console.error('MongoDB connection error:', err));
 
 
 
