cff6581a801051fe2a5ac3f5f47705d2463c68e1
Rakesh Baddipadaga <rakeshbaddipadaga8@gmail.com>
2024-05-30 08:48:16 +0530

    message

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use('/api/profile', userRoutes);
 app.use('/api/image', imageRoutes);
 app.use('/api/vcard',vcardRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI,)
   .then(() => {
     console.log('MongoDB connected');
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
