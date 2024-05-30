be7ac24b75ddf5d77af256d0add600c0cd698f4a
Rakesh Baddipadaga <rakeshbaddipadaga8@gmail.com>
2024-05-30 08:45:27 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -17,14 +17,14 @@ app.use('/api/image', imageRoutes);
 app.use('/api/vcard',vcardRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {console.log('MongoDB connected')
-
+  .then(() => {
+    console.log('MongoDB connected');
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 })
 .catch(err => console.error('MongoDB connection error:', err));
 
 
-
+module.exports = app;
 
 
 
